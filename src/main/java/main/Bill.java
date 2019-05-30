d79135618b79a1ac9c4a80ad313ca83cbe350103
Hannan <Hannan@Hannan-PC>
2019-05-30 10:02:12 +0530

    Completed Successfully

--- src/main/java/main/Bill.java
+++ src/main/java/main/Bill.java
@@ -1,7 +1,7 @@
 package main;
 import java.util.Date;
 import java.util.Random;
-import java.util.Scanner;
+
 
 public class Bill extends CustomerName{
 	 //bill no & date
@@ -10,20 +10,20 @@ public class Bill extends CustomerName{
         // It wishopTestll generate 6 digit random Number.
         // from 0 to 999999
         Random rnd = new Random();
-        int number = rnd.nextInt(999999);
-    
+int number = rnd.nextInt(999999);
+
         // this will convert any number sequence into 6 character.
         return String.format("%06d", number);
     }
     String billNo;
     String date;
     public void getBill(String name){
-        billNo = getRandomNumberString();
+billNo = getRandomNumberString();
         Date d = new Date();
         date = d.toString();
-        System.out.println('\n');
-        System.out.println("-------------------------------------------------------");
-        System.out.println("|  Bill No: "+billNo+" "+date+'\n');
-        System.out.println("|  Customers Name: "+ name+'\n');
+System.out.println('\n');
+System.out.println("-------------------------------------------------------");
+System.out.println("|  Bill No: "+billNo+" "+date+'\n');
+System.out.println("|  Customers Name: "+ name+'\n');
     }
 }
